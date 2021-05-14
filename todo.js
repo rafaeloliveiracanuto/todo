8f79ec3bf81fb560986c620257bc67d4fa8fa5bf
Dominic <ppkk.dominic@gmail.com>
2021-05-14 15:19:08 +0100

    Add html and styles

---
+++ todo.js
@@ -0,0 +1,12 @@
+const todoItems = [];
+
+function addTodo(text) {
+  const todo = {
+    text,
+    checked: false,
+    id: Date.now(),
+  };
+
+  todoItems.push(todo);
+  console.log(todoItems);
+}
